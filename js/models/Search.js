46eb4b6378dbdb193f1832c780d9fdc51dd4da1f
FabienStervinou <stervinoufabien@gmail.com>
2021-12-26 22:34:42 +0100

    WIP Search event onKeyUp

--- js/models/Search.js
+++ js/models/Search.js
@@ -1,5 +1,29 @@
 export default class Search {
   constructor () {
-    this.input = document.querySelector('form')
+    this.isSearchActive = false
+    this.data = null
+  }
+
+  init (data) {
+    const searchDOM = document.querySelector('#search')
+
+    if (searchDOM) {
+      searchDOM.addEventListener('keyup', e =>{
+        this.onSearchKeyUp(e)
+      })
+    }
+
+    this.data = data
+
+    console.log('Search init this.data : ', this.data);
+  }
+
+  onSearchKeyUp (e) {
+    if (e.target.value.length >= 3) {
+      this.isSearchActive = true
+      console.log("Show result");
+    } else {
+      this.isSearchActive = false
+    }
   }
 }
