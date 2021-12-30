73d29128c1bfe49177809613776ae3382dbcfcb2
FabienStervinou <stervinoufabien@gmail.com>
2021-12-30 21:27:13 +0100

    Search params comment

--- js/models/Search.js
+++ js/models/Search.js
@@ -4,24 +4,31 @@ export default class Search {
     this.data = null
   }
 
+  /**
+   * 
+   * @param {Array} data 
+   */
   init (data) {
     const searchDOM = document.querySelector('#search')
 
     if (searchDOM) {
-      searchDOM.addEventListener('keyup', e =>{
+      searchDOM.addEventListener('keyup', e => {
         this.onSearchKeyUp(e)
       })
     }
 
     this.data = data
-
-    console.log('Search init this.data : ', this.data);
   }
 
+  /**
+   * 
+   * @param {Event} e 
+   */
   onSearchKeyUp (e) {
     if (e.target.value.length >= 3) {
       this.isSearchActive = true
       console.log("Show result");
+      this.updateRecipesSearch(e.target.value)
     } else {
       this.isSearchActive = false
     }
