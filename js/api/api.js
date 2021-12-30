78540cce05fee8e483b53154b87b2b66c498bd7d
FabienStervinou <stervinoufabien@gmail.com>
2021-12-30 21:04:03 +0100

    Recipes API script

---
+++ js/api/api.js
@@ -0,0 +1,18 @@
+export default class Api {
+  /**
+   * 
+   * @param {string} url 
+   */
+  constructor(url) {
+      this._url = url
+  }
+
+  async getRecipes() {
+    return fetch(this._url)
+        .then(res => res.json())
+        .then(res => {
+          return res.recipes
+        })
+        .catch(err => console.error('an error occurs', err))
+  }
+}
