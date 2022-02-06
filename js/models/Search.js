249f12f1e984049be3578a1b818a2783909377ca
fstervin <fstervin@prismamedia.com>
2022-02-06 11:38:14 +0100

    test

--- js/models/Search.js
+++ js/models/Search.js
@@ -49,7 +49,7 @@ export default class Search {
    * @param {String} value
    */
   updateRecipesSearch(value) {
-
+    console.log('log updateRecipesSearch')
     value.trim()
 
     const isTagActive = !!document.querySelector('.tag')
